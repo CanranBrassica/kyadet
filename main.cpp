2be306afb91464192e4e02d266952cc8d0502752
CanranBrassica <kyabetsu.3141@gmail.com>
2018-03-23 05:18:22 +0900

    diffは式を返す様にした

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <cmath>
 
 namespace kyadet
 {
@@ -10,20 +11,20 @@ struct Parameter {
 template <class T>
 class Input : public Parameter<Input<T>>
 {
-    std::shared_ptr<T> t_ = nullptr;
+    std::unique_ptr<T> t_ = nullptr;
 
 public:
     explicit Input(T t)
-        : t_(std::make_shared<T>(t)) {}
+        : t_(std::make_unique<T>(t)) {}
 
-    T value() const
+    auto value() const
     {
         return *t_;
     }
 
-    T diff(const std::shared_ptr<void>& p) const
+    auto diff(const std::shared_ptr<void>& p) const
     {
-        return this == p.get() ? T(1) : T(0);
+        return this == p.get() ? std::make_shared<Input<T>>(1) : std::make_shared<Input<T>>(0);
     }
 };  //class Input
 
@@ -37,19 +38,16 @@ public:
     explicit Add(const std::shared_ptr<L>& l, const std::shared_ptr<R>& r)
         : l_(l), r_(r)
     {
-        std::cout << "どっちもshared_ptrなAdd constructer" << std::endl;
     }
 
     explicit Add(L&& l, const std::shared_ptr<R>& r)
         : l_(std::make_shared<L>(l)), r_(r)
     {
-        std::cout << "L&&なAdd constructer" << std::endl;
     }
 
     explicit Add(const std::shared_ptr<L>& l, R&& r)
         : l_(l), r_(std::make_shared<R>(r))
     {
-        std::cout << "R&&なAdd constructer" << std::endl;
     }
 
     auto value() const
@@ -69,26 +67,81 @@ template <class L, class R,
     std::enable_if_t<std::is_base_of_v<Parameter<R>, R>, std::nullptr_t> = nullptr>
 auto operator+(const std::shared_ptr<L>& l, const std::shared_ptr<R>& r)
 {
-    std::cout << "operator+ & &" << std::endl;
     return std::make_shared<Add<L, R>>(l, r);
 }
 
 template <class L, class R,
     std::enable_if_t<std::is_arithmetic_v<L>, std::nullptr_t> = nullptr,
     std::enable_if_t<std::is_base_of_v<Parameter<R>, R>, std::nullptr_t> = nullptr>
-auto operator+(L&& l, const std::shared_ptr<R>& r) -> std::shared_ptr<Add<Input<L>, R>>
+auto operator+(L&& l, const std::shared_ptr<R>& r)
 {
-    std::cout << "operator+ && &" << std::endl;
     return std::make_shared<Add<Input<L>, R>>(Input<L>{l}, r);
 }
 
 template <class L, class R,
     std::enable_if_t<std::is_base_of_v<Parameter<L>, L>, std::nullptr_t> = nullptr,
     std::enable_if_t<std::is_arithmetic_v<R>, std::nullptr_t> = nullptr>
-auto operator+(const std::shared_ptr<L>& l, R&& r) -> std::shared_ptr<Add<L, Input<R>>>
+auto operator+(const std::shared_ptr<L>& l, R&& r)
 {
-    std::cout << "operator+ && &" << std::endl;
     return std::make_shared<Add<L, Input<R>>>(l, Input<R>(r));
+}
+
+template <class L, class R>
+class Sub : public Parameter<Sub<L, R>>
+{
+    std::shared_ptr<L> l_ = nullptr;
+    std::shared_ptr<R> r_ = nullptr;
+
+public:
+    explicit Sub(const std::shared_ptr<L>& l, const std::shared_ptr<R>& r)
+        : l_(l), r_(r)
+    {
+    }
+
+    explicit Sub(L&& l, const std::shared_ptr<R>& r)
+        : l_(std::make_shared<L>(l)), r_(r)
+    {
+    }
+
+    explicit Sub(const std::shared_ptr<L>& l, R&& r)
+        : l_(l), r_(std::make_shared<R>(r))
+    {
+    }
+
+    auto value() const
+    {
+        return l_->value() - r_->value();
+    }
+
+    auto diff(const std::shared_ptr<void>& p) const
+    {
+        return l_->diff(p) - r_->diff(p);
+    }
+
+};  //class Sub
+
+template <class L, class R,
+    std::enable_if_t<std::is_base_of_v<Parameter<L>, L>, std::nullptr_t> = nullptr,
+    std::enable_if_t<std::is_base_of_v<Parameter<R>, R>, std::nullptr_t> = nullptr>
+auto operator-(const std::shared_ptr<L>& l, const std::shared_ptr<R>& r)
+{
+    return std::make_shared<Sub<L, R>>(l, r);
+}
+
+template <class L, class R,
+    std::enable_if_t<std::is_arithmetic_v<L>, std::nullptr_t> = nullptr,
+    std::enable_if_t<std::is_base_of_v<Parameter<R>, R>, std::nullptr_t> = nullptr>
+auto operator-(L&& l, const std::shared_ptr<R>& r)
+{
+    return std::make_shared<Sub<Input<L>, R>>(Input<L>{l}, r);
+}
+
+template <class L, class R,
+    std::enable_if_t<std::is_base_of_v<Parameter<L>, L>, std::nullptr_t> = nullptr,
+    std::enable_if_t<std::is_arithmetic_v<R>, std::nullptr_t> = nullptr>
+auto operator-(const std::shared_ptr<L>& l, R&& r)
+{
+    return std::make_shared<Sub<L, Input<R>>>(l, Input<R>(r));
 }
 
 template <class L, class R>
@@ -101,19 +154,16 @@ public:
     explicit Mult(const std::shared_ptr<L>& l, const std::shared_ptr<R>& r)
         : l_(l), r_(r)
     {
-        std::cout << "どっちもshared_ptrなMult constructer" << std::endl;
     }
 
     explicit Mult(L&& l, const std::shared_ptr<R>& r)
         : l_(std::make_shared<L>(l)), r_(r)
     {
-        std::cout << "L&&なMult constructer" << std::endl;
     }
 
     explicit Mult(const std::shared_ptr<L>& l, R&& r)
         : l_(l), r_(std::make_shared<R>(r))
     {
-        std::cout << "R&&なMult constructer" << std::endl;
     }
 
     auto value() const
@@ -123,7 +173,7 @@ public:
 
     auto diff(const std::shared_ptr<void>& p) const
     {
-        return l_->diff(p) * r_->value() + l_->value() * r_->diff(p);
+        return l_->diff(p) * r_ + l_ * r_->diff(p);
     }
 
 };  //class Mult
@@ -133,26 +183,113 @@ template <class L, class R,
     std::enable_if_t<std::is_base_of_v<Parameter<R>, R>, std::nullptr_t> = nullptr>
 auto operator*(const std::shared_ptr<L>& l, const std::shared_ptr<R>& r)
 {
-    std::cout << "operator* & &" << std::endl;
     return std::make_shared<Mult<L, R>>(l, r);
 }
 
 template <class L, class R,
     std::enable_if_t<std::is_arithmetic_v<L>, std::nullptr_t> = nullptr,
     std::enable_if_t<std::is_base_of_v<Parameter<R>, R>, std::nullptr_t> = nullptr>
-auto operator*(L&& l, const std::shared_ptr<R>& r) -> std::shared_ptr<Mult<Input<L>, R>>
+auto operator*(L&& l, const std::shared_ptr<R>& r)
 {
-    std::cout << "operator* && &" << std::endl;
     return std::make_shared<Mult<Input<L>, R>>(Input<L>{l}, r);
 }
 
 template <class L, class R,
     std::enable_if_t<std::is_base_of_v<Parameter<L>, L>, std::nullptr_t> = nullptr,
     std::enable_if_t<std::is_arithmetic_v<R>, std::nullptr_t> = nullptr>
-auto operator*(const std::shared_ptr<L>& l, R&& r) -> std::shared_ptr<Mult<L, Input<R>>>
+auto operator*(const std::shared_ptr<L>& l, R&& r)
 {
-    std::cout << "operator* && &" << std::endl;
     return std::make_shared<Mult<L, Input<R>>>(l, Input<R>(r));
+}
+template <class L, class R>
+class Div : public Parameter<Div<L, R>>
+{
+    std::shared_ptr<L> l_ = nullptr;
+    std::shared_ptr<R> r_ = nullptr;
+
+public:
+    explicit Div(const std::shared_ptr<L>& l, const std::shared_ptr<R>& r)
+        : l_(l), r_(r)
+    {
+    }
+
+    explicit Div(L&& l, const std::shared_ptr<R>& r)
+        : l_(std::make_shared<L>(l)), r_(r)
+    {
+    }
+
+    explicit Div(const std::shared_ptr<L>& l, R&& r)
+        : l_(l), r_(std::make_shared<R>(r))
+    {
+    }
+
+    auto value() const
+    {
+        return l_->value() / r_->value();
+    }
+
+    auto diff(const std::shared_ptr<void>& p) const
+    {
+        return (l_->diff(p) * r_ - l_ * r_->diff(p)) / r_ / r_;
+    }
+
+};  //class Div
+
+template <class L, class R,
+    std::enable_if_t<std::is_base_of_v<Parameter<L>, L>, std::nullptr_t> = nullptr,
+    std::enable_if_t<std::is_base_of_v<Parameter<R>, R>, std::nullptr_t> = nullptr>
+auto operator/(const std::shared_ptr<L>& l, const std::shared_ptr<R>& r)
+{
+    return std::make_shared<Div<L, R>>(l, r);
+}
+
+template <class L, class R,
+    std::enable_if_t<std::is_arithmetic_v<L>, std::nullptr_t> = nullptr,
+    std::enable_if_t<std::is_base_of_v<Parameter<R>, R>, std::nullptr_t> = nullptr>
+auto operator/(L&& l, const std::shared_ptr<R>& r)
+{
+    return std::make_shared<Div<Input<L>, R>>(Input<L>{l}, r);
+}
+
+template <class L, class R,
+    std::enable_if_t<std::is_base_of_v<Parameter<L>, L>, std::nullptr_t> = nullptr,
+    std::enable_if_t<std::is_arithmetic_v<R>, std::nullptr_t> = nullptr>
+auto operator/(const std::shared_ptr<L>& l, R&& r)
+{
+    return std::make_shared<Div<L, Input<R>>>(l, Input<R>(r));
+}
+
+template <class T>
+class Exp : public Parameter<Exp<T>>
+{
+    std::shared_ptr<T> t_ = nullptr;
+
+public:
+    explicit Exp(const std::shared_ptr<T>& t)
+        : t_(t)
+    {
+    }
+
+    explicit Exp(T&& t)
+        : t_(std::make_shared<T>(t))
+    {
+    }
+
+    auto value() const
+    {
+        return std::exp(t_->value());
+    }
+
+    auto diff(const std::shared_ptr<void>& p) const
+    {
+        return exp(t_) * t_->diff(p);
+    }
+};  //class Exp
+
+template <class T, std::enable_if_t<std::is_base_of_v<Parameter<T>, T>, std::nullptr_t> = nullptr>
+auto exp(std::shared_ptr<T> t)
+{
+    return std::make_shared<Exp<T>>(t);
 }
 
 }  //namespace kyadet
@@ -160,10 +297,10 @@ auto operator*(const std::shared_ptr<L>& l, R&& r) -> std::shared_ptr<Mult<L, In
 int main()
 {
     using namespace kyadet;
-    auto x1 = std::make_shared<Input<int>>(1);
-    auto x2 = std::make_shared<Input<int>>(1);
-    auto y = x1 * x1 + x1 + x2 * x1;
-    std::cout << y->value() << std::endl;
-    std::cout << y->diff(x1) << std::endl;
+    auto x = std::make_shared<Input<double>>(1);
+    auto y = std::make_shared<Input<double>>(2);
+    auto z = x / y;
+    std::cout << z->diff(x)->value() << std::endl;
+    std::cout << z->diff(x)->diff(y)->value() << std::endl;
     return 0;
 }
